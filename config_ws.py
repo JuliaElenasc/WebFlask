c0f51009ec1c979a70dc5bf10504fa5b0923f2c9
JuliaElenaSC <julia.elena.sc@gmail.com>
2023-06-23 22:38:57 +0200

    sistemazione del post e invio del id del device nel formulario

--- config_ws.py
+++ config_ws.py
@@ -1,44 +1,95 @@
 #configurazione nel server # Patron DAO (Data Acces Object, per mantenere la persistenza dei dati)
-
 from flask import Flask
 from flask_mysqldb import MySQL
 
 app = Flask(__name__) # Crea una instancia de la aplicación Flask con el nombre app para el modulo actual
+#app.coonfig['SECRET_KEY']='esto es un secreto'
 app.config['MYSQL_HOST'] = 'localhost' #Establece la configuración para el host de la base de datos MySQL
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'flaskregistro'
+app.config['MYSQL_DB'] = 'flask'
 mysql = MySQL(app)
 
 class DevelopmentConfig:
     DEBUG=True # Para mostrar mensajes detallados y reiniciar automáticamente la aplicación cuando se detectan cambios.
 
 class User: 
-    def __init__(self, username, password, nascita, corso): # Se ejecuta automáticamente cuando se crea una nueva instancia de la clase.
-        self.username = username #atributos de la clase
+    def __init__(self, username, password, nascita, corso,id): # Se ejecuta automáticamente cuando se crea una nueva instancia de la clase.
+        self.username = username #atributos de la clase (publicos por defecto)
         self.password = password
         self.nascita = nascita
         self.corso = corso
+        self.id=id
+
+class Device:
+    def __init__(self, id,device, stanza):
+        self.id=id
+        self.device = device
+        self.stanza = stanza
+
+class Action:
+    def __init__(self, value_state, intensity, battery, date, id_Dev, id_user):
+        self.value_state = value_state
+        self.intensity = intensity
+        self.battery = battery
+        self.date = date
+        self.id_Dev = id_Dev
+        self.id_user = id_user
+       
 
 class DataBase:
     @staticmethod # es independiente y se puede llamar directamente desde la clase sin necesidad de crear una instancia
     def insert_user(user):
-        sql = 'INSERT INTO registro (user, password, nascita, corso) VALUES (%s, %s, %s, %s)'#insertar con marcadores de posicion
+        sql = 'INSERT INTO user (user, password, nascita, corso) VALUES (%s, %s, %s, %s)'#insertar con marcadores de posicion
         values = (user.username, user.password, user.nascita, user.corso) # valores a insertar
         DataBase.execute_query(sql, values)# ejecuta la consulta
     
     @staticmethod # Falta definir la logica de negocio (considerar casos: quien, cuando, como)
     def delete_user(username):
-        sql = 'DELETE FROM registro WHERE user = %s'
+        sql = 'DELETE FROM user WHERE user = %s'
         values = (username,)
         DataBase.execute_query(sql, values)
 
     @staticmethod 
     def obtain_user(username, password):
-        sql = 'SELECT * FROM registro WHERE user = %s AND password = %s'
+        sql = 'SELECT * FROM user WHERE user = %s AND password = %s'
         values = (username, password)
         return DataBase.execute_query(sql, values, fetch_one=True)
     
+    @staticmethod
+    def obtain_device():
+        sql = 'SELECT * FROM device'
+        devices = DataBase.execute_query(sql, fetch_one=False)
+        print("DataBase obtain_device ", devices)
+        return devices
+    
+    @staticmethod
+    def control_device(device):
+        sql = "INSERT INTO actions(value_state, intensity, battery,id_Dev,id_user) VALUES (%s, %s, %s,%s,%s)"
+        values = (device.value_state, device.intensity, device.battery,device.id_Dev, device.id_user)
+        return DataBase.execute_query(sql, values, fetch_one=True)
+
+    @staticmethod
+    def getDeviceById(id):
+        sql= "SELECT * FROM device WHERE id = %s"
+        values=(id,)
+        result= DataBase.execute_query(sql,values,fetch_one=True)
+        if result is not None:
+            device = {
+                'id': result[0],
+                'device': result[1],
+                'stanza': result[2]
+             }
+            print(device)
+            return device
+        else:
+            return None
+        
+          
+
+    
+
+
     @staticmethod
     def execute_query(sql, values=None, fetch_one=False):
         cur = mysql.connection.cursor() #crea un cursor, para ejecutar comandos y recuperar info de la BD
@@ -53,13 +104,13 @@ class DataBase:
     
     @staticmethod
     def query(sql):
-        cur = DataBase.conect() #poner un get?--> revisar bibliografia
+        cur = DataBase.connect() #poner un get?--> revisar bibliografia
         cur.execute(sql)# ejecuta la consulta
         mysql.connection.commit()#guarda la consulta --> revisar
         DataBase.close(cur)
     
     @staticmethod
-    def conect():
+    def connect():
         return mysql.connection.cursor()
     
     @staticmethod
--- webserver.py
+++ webserver.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
-from config_ws import User, app, DevelopmentConfig , DataBase
+from config_ws import User, app, DataBase,Device,Action
+from datetime import datetime
 
 @app.route('/')
 def home():
@@ -11,6 +12,11 @@ def login():
         user = request.form['user']
         password = request.form['password']
         data = DataBase.obtain_user(user, password)
+        dataDev = DataBase.obtain_device()
+
+        listDevice = []
+        for itemDev in dataDev:
+            listDevice.append(Device(itemDev[0], itemDev[1], itemDev[2]))
 
         if data:
             dettaglioUtente = {
@@ -19,8 +25,9 @@ def login():
                 "nascita": data[3],
                 "corso": data[4]
             }
-            return render_template('dettaglio.html', dettaglioUtente=dettaglioUtente)
-            #return redirect("/detaglio") --> respuesta 302
+            print("WebServer login", dataDev)
+
+            return render_template('dettaglio.html', dettaglioUtente=dettaglioUtente, listDevice = listDevice)
         else:
             flash('Credenziali errate, prova di nuovo')
             return redirect(url_for('login'))
@@ -41,14 +48,61 @@ def add_registro():
         usuario = User(user, password, nascita, corso)
         DataBase.insert_user(usuario)
         return render_template('dettaglio.html', dettaglioUtente=usuario)
-        
-@app.route('/detaglio')
+
+@app.route('/dettaglio')
 def dettaglio():
     data = DataBase.obtain_user()
     return render_template('dettaglio.html', dettaglioUtente=data)
+    
+
+@app.route('/device', methods=['GET','POST'])
+def add_action():
+    if request.method == 'GET':
+        device_id = request.args.get('deviceId')
+        
+        if device_id is not None: 
+            id = int(device_id)
+        else: id = None
+        
+        device = DataBase.getDeviceById(id)
+        if device:
+            objDevice = Device(device.get('id'), device.get('device'), device.get('stanza'))
+            
+            return render_template("device.html", device=objDevice)
+        else:
+            
+            return "Device not found"
+            #creare istanza della classe device in base ai dati ottenuti dal DB (come fatto in listDevice di Login)
+
+    elif request.method=='POST':
+       
+       date= datetime.now()
+       id_Dev = request.form.get('deviceId')
+       intensity = request.form.get('brightness')
+       battery= 100-(intensity* 2)
+       id_user = 1
+       value_state = request.form.get('check')
+       
+       if value_state is None:
+            value_state = "0"
+       else: value_state="1"
+       data=Action(value_state,intensity,battery,date, id_Dev, id_user)
+       DataBase.control_device(data)
+       return redirect('/device?deviceId='+str(id_Dev))
+    else:
+        return render_template("device.html")
+    
 
 if __name__ == '__main__':
     try:
-        app.run(debug=True)
+        app.run(debug=True,host="0.0.0.0")
     except KeyboardInterrupt:
         DataBase.close()
+
+
+# app.route(/get reg",methods post)
+#def post registrazione()
+#nome=request.form[user]
+#Unir registro y add registro
+#Crear en vez de flash un otro html else reurn redirect al endpoint return(redirect"/passerror")
+# 
